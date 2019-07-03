d47f6556db66ab06fed5ce352480d6a0440c2b85
Gnanadinesh <balakarthi13@gmail.com>
2019-07-03 09:30:49 +0530

    dear

---
+++ src/org/pack/Veralevel.java
@@ -0,0 +1,15 @@
+package org.pack;
+
+public class Veralevel {public static void main(String[] args) {
+	
+	String s="9Greens@123%";
+	String s1 = s.substring(7, 8);
+	String s2 = s.substring(11, 12);
+	System.out.println(s1+s2);
+		String d= (s1+s2);
+	int length = d.length();
+	System.out.println(length);
+
+}
+
+}
